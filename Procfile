96d34b41b791d4fc9a849cdd2e99e3d026981007
rushi-creates <rushi.creates@gmail.com>
2022-04-30 14:32:09 +0530

    modified procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py migrate 
-web: gunicorn myproject.wsgi --log-files -
+release: python manage.py migrate
+web: gunicorn myproject.wsgi --log-file -
--- core/views.py
+++ core/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
+
 # Create your views here.
 # ---------------------------------------------------------------------------- #
 #                                   //! Auth                                   #
